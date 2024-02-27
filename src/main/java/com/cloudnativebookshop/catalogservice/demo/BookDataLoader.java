d9b7abc2084fc24496cabd16a36193efe0ade45c
Shant Dashjian <shantgdashjian@gmail.com>
2024-02-27 21:53:42 +0400

    Turn the domain entity into persistence entity

--- src/main/java/com/cloudnativebookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/cloudnativebookshop/catalogservice/demo/BookDataLoader.java
@@ -18,8 +18,8 @@ public class BookDataLoader {
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void loadBookTestData() {
-		var book1 = new Book("1234567891", "Cloud Native Spring in Action", "Thomas Vitale", 29.99);
-		var book2 = new Book("1234567892", "Spring in Action", "Craig Walls", 19.99);
+		var book1 = Book.of("1234567891", "Cloud Native Spring in Action", "Thomas Vitale", 29.99);
+		var book2 = Book.of("1234567892", "Spring in Action", "Craig Walls", 19.99);
 		bookRepository.save(book1);
 		bookRepository.save(book2);
 	}
