5173a83d3a2c2b090cf8c91c695cc6b9af632bb6
Shant Dashjian <shantgdashjian@gmail.com>
2024-02-27 22:10:59 +0400

    Add createdDate and lastModifiedDate auditing

--- src/main/java/com/cloudnativebookshop/catalogservice/domain/Book.java
+++ src/main/java/com/cloudnativebookshop/catalogservice/domain/Book.java
@@ -4,8 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
+
+import java.time.Instant;
 
 public record Book(
 
@@ -26,6 +30,12 @@ public record Book(
 	@Positive(message = "The book price must be greater than zero.")
 	Double price,
 
+	@CreatedDate
+	Instant createdDate,
+
+	@LastModifiedDate
+	Instant lastModifiedDate,
+
 	@Version
 	int version
 ) {
@@ -33,7 +43,7 @@ public record Book(
 		String isbn, String title, String author, Double price
 	) {
 		return new Book(
-			null, isbn, title, author, price, 0
+			null, isbn, title, author, price, null, null, 0
 		);
 	}
 }
